c4c23ae4f694726dfb02586dab73db276b8de79a
empty666 <empty666@me.com>
2023-03-21 18:42:47 +0900

    [BS-0007] Exception Handler 수정

--- searcher-common/src/main/java/com/empty/searcher/common/base/constants/SearcherConstants.java
+++ searcher-common/src/main/java/com/empty/searcher/common/base/constants/SearcherConstants.java
@@ -3,4 +3,5 @@ package com.empty.searcher.common.base.constants;
 public class SearcherConstants {
 
     public static final String NOT_VALID_ARGUMENTS = "요청 한 파라미터가 올바르지 않습니다.";
+    public static final String INTERNAL_SERVER_ERROR_MESSAGE = "처리 중 오류가 발생했습니다.";
 }
--- searcher-common/src/main/java/com/empty/searcher/common/base/exception/handler/RestExceptionHandler.java
+++ searcher-common/src/main/java/com/empty/searcher/common/base/exception/handler/RestExceptionHandler.java
@@ -7,14 +7,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.empty.searcher.common.base.constants.SearcherConstants.INTERNAL_SERVER_ERROR_MESSAGE;
 import static com.empty.searcher.common.base.constants.SearcherConstants.NOT_VALID_ARGUMENTS;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Slf4j
 @ControllerAdvice
@@ -28,8 +31,12 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
             .map(x -> x.getDefaultMessage())
             .collect(Collectors.toList());
 
-        log.info("Errors : {}", errors);
-
         return new ResponseEntity<>(ErrorResponse.from(NOT_VALID_ARGUMENTS, errors), BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = { Exception.class })
+    public final ResponseEntity<Object> handleInternalServerError(RuntimeException ex, WebRequest request) {
+        log.info("INTERNAL_SERVER_ERROR_MESSAGE", ex);
+        return new ResponseEntity<>(ErrorResponse.from(INTERNAL_SERVER_ERROR_MESSAGE, ex.getMessage()), INTERNAL_SERVER_ERROR);
     }
 }
